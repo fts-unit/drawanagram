05bcef9761f16ef9a601997aee70ef2bf9ddbf40
fts-unit <fts.unit@gmail.com>
2018-11-16 13:30:49 +0900

    PWAキャッシュ対応

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,60 @@
 // service-worker.js
+var CACHE_STATIC_VERSION = 'static-v1';
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+
+  event.waitUntil(
+    caches.open(CACHE_STATIC_VERSION)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App...');
+        cache.addAll([
+          '/',
+          '/src/',
+          '/img/',
+          '/img/chars/',
+        ]);
+      })
+  );
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_VERSION && key !== CACHE_DYNAMIC_VERSION) {
+            console.log('[Service Worker] Removing old cache...');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {});
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetch');
+  event.respondWith(
+    // キャッシュの存在チェック
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+            });
+        }
+      })
+  );});
